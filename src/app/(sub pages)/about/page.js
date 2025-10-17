8707a1e77326adafcb89c5e9848951a0dd25ecbd
Hamza Gačić <hamza.gacic.22@size.ba>
2025-10-17 21:09:18 +0200

    fix

--- src/app/(sub pages)/about/page.js
+++ src/app/(sub pages)/about/page.js
@@ -30,7 +30,7 @@ export default function Home() {
       </div>
 
       <div className="relative w-full h-screen flex flex-col items-center justify-center">
-        <div className="absolute flex flex-col items-center text-center top-1/2 sm:top-[60%] left-1/2 -translate-y-1/2 -translate-x-1/2">
+        <div className="flex flex-col items-center text-center">
           <h1 className="font-bold  text-6xl xs:text-7xl sm:text-8xl  lg:text-9xl text-accent">
             Hamza Gačić
           </h1>
